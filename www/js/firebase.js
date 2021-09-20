47bd3461af429be452e052cbd4d1761f586391c0
Ben Almeida <48792648+BenAlmeida@users.noreply.github.com>
2021-09-20 02:55:50 -0300

    ta funcionando!!!

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	  apiKey: "AIzaSyCdIlF0BNgvIxuCSt898EfuvoH9kiGLvJQ",
+	authDomain: "appagendamentos-d42ac.firebaseapp.com",
+	projectId: "appagendamentos-d42ac",
+	storageBucket: "appagendamentos-d42ac.appspot.com",
+	messagingSenderId: "422371342135",
+	appId: "1:422371342135:web:1fe1f5b7f02d9a0b5b0cb2",
+	measurementId: "G-ELF0GPBZ5F"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
